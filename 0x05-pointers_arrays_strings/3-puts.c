f145b98c1f4eafb9d9a1137befc8267cb756c83a
InvestorWhale <razehappy@gmail.com>
2022-12-20 00:37:54 -0800

    task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,9 @@
 #include"main.h"
 
 /**
- *  _puts - prints a string, followed by a newline to stdout
- * @str: string parameter to print
+ * _puts - print a string to stdout
  * @str: char array string type
+ * Description: Can only use _putchar
  */
 
 void _puts(char *str)
