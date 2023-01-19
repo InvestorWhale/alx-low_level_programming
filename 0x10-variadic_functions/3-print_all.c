3abbd2d83fe76b9b669ea21cc04c59c7643826fa
InvestorWhale <razehappy@gmail.com>
2023-01-19 15:09:32 -0800

    task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * #include "variadic_functions.h"
+ * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
  *
  * Return: no return.
