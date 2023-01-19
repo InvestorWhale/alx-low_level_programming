a0a934206668975efa3ba8078a6c00b833e3f476
InvestorWhale <razehappy@gmail.com>
2023-01-19 15:05:53 -0800

    task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything.
+ * #include "variadic_functions.h"
  * @format: a list of types of arguments passed to the function.
  *
  * Return: no return.
@@ -24,29 +24,29 @@ void print_all(const char * const format, ...)
 				printf(", ");
 				break;
 			} j++;
+
 		}
-	}
-	switch (format[i])
-	{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(valist, int)), c = 1;
 				break;
-			}
-			printf("%s", str);
-			break;
-	} i++;
-}
-printf("\n"), va_end(valist);
+			case 'i':
+				printf("%d", va_arg(valist, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double)), c = 1;
+				break;
+			case 's':
+				str = va_arg(valist, char *), c = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
 }
